f027d09764f9acf266a9aeab042ad1dfe98b4c4c
B3NK12 <127891518+B3NK12@users.noreply.github.com>
2025-04-03 13:26:57 -0400

    Update manageUsers.php
    Maybe works (👍

--- manageUsers.php
+++ manageUsers.php
@@ -21,23 +21,29 @@
         echo "<img src="https://s3-us-west-2.amazonaws.com/scorestream-team-profile-pictures/285522/20181011000648_310_mascot1280Near.png" alt="Logo" class="logo">";
         <a href="logout.php"><button type="button" class="btn">Logout</button></a>
         <div class="container">
-		    <div class="form_area">
-		    <div class="title">Administrative Users</div>
-			foreach ($admins as $admin) {
-                $Email = $admin['email'];
-                $Password = $admin['password'];
-                $Salt = $admin['salt'];
-                $UserStatus = $admin['UserStatus'];
-                $Gender = $admin['Gender'];
+	    <div class="form_area">
+		<div class="title">Administrative Users</div>
+		foreach ($admins as $admin) {
+                    $Email = $admin['email'];
+                    $Password = $admin['password'];
+                    $Salt = $admin['salt'];
+                    $UserStatus = $admin['UserStatus'];
+                    $Gender = $admin['Gender'];
 
-                // Display the functionality for each user
-                echo "<div class="user">"
-                echo "</div>"
+                    // Display the functionality for each user, display email, password, userstatus, and gender is editable texts with an update button in a row
+                    echo "<div class="user">";
+    		    echo "<input type="email" id="email" value="$Email" size="25">";
+		    echo "<input type="text" id="password" value="$Password" size="25">";
+		    echo "<input type="text" id="userStatus" value="$UserStatus" size="25">";
+		    echo "<input type="text" id="gender" value="$Gender" size="25">";
+                    echo "</div>";
                 
-                echo "<div><h3>Year: $carYear</h3><h3>Brand: $carBrand</h3><h3>Model: $carModel</h3><h3>Location: $carLocation</h3></div>"; //Example echo line
-            }
-		    </div>
+                    //echo "<div><h3>Year: $carYear</h3><h3>Brand: $carBrand</h3><h3>Model: $carModel</h3><h3>Location: $carLocation</h3></div>"; //Example echo line
+		    //echo '<h2>' . htmlspecialchars($product['name']) . '</h2>';
+
+            	}
 	    </div>
+	</div>
         
 
         // Fetch all users from the Database
@@ -46,7 +52,29 @@
         $users = $stmt->fetch();
 
         // Display all users and functionality
-        
+        <div class="container">
+	    <div class="form_area">
+		<div class="title">Users</div>
+		foreach ($users as $user) {
+                    $Email = $user['email'];
+                    $Password = $user['password'];
+                    $Salt = $user['salt'];
+                    $UserStatus = $user['UserStatus'];
+                    $Gender = $user['Gender'];
+
+                    // Display the functionality for each user, display email, password, userstatus, and gender is editable texts with an update button in a row
+                    echo "<div class="user">";
+    		    echo "<input type="email" id="email" value="$Email" size="25">";
+		    echo "<input type="text" id="password" value="$Password" size="25">";
+		    echo "<input type="text" id="userStatus" value="$UserStatus" size="25">";
+		    echo "<input type="text" id="gender" value="$Gender" size="25">";
+                    echo "</div>";
+                
+                    //echo "<div><h3>Year: $carYear</h3><h3>Brand: $carBrand</h3><h3>Model: $carModel</h3><h3>Location: $carLocation</h3></div>"; //Example echo line
+		    //echo '<h2>' . htmlspecialchars($product['name']) . '</h2>';
+            	}
+	    </div>
+	</div>
     ?>
 </body>
 </html>
