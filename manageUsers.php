e17f6bdb9b53ab44ba9138c3e9a7d780eb78a499
B3NK12 <127891518+B3NK12@users.noreply.github.com>
2025-04-03 12:52:52 -0400

    Update manageUsers.php
    Not done

--- manageUsers.php
+++ manageUsers.php
@@ -1,1 +1,40 @@
 // This is for the admin to manage the users on the website
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manage Users</title>
+</head>
+<body>
+    <?php
+        // Database connection
+        $conn = new PDO("mysql:host=localhost;dbname=survey_db", 'root', '');
+
+        // Fetch all admins from the Database
+        $stmt = $conn->prepare("SELECT * FROM users WHERE UserStatus='admin'");
+        $stmt->execute();
+        $admins = $stmt->fetch();
+
+        // Display admins and functionality
+            // echo image
+            // echo header
+        foreach ($admins as $admin) {
+            $Email = $admin['email'];
+            $Password = $admin['password'];
+            $Salt = $admin['salt'];
+            $UserStatus = $admin['UserStatus'];
+            $Gender = $admin['Gender'];
+
+            echo "<div><h3>Year: $carYear</h3><h3>Brand: $carBrand</h3><h3>Model: $carModel</h3><h3>Location: $carLocation</h3></div>"; //Example echo line
+        }
+
+        // Fetch all users from the Database
+        $stmt = $conn->prepare("SELECT * FROM users WHERE UserStatus='user'");
+        $stmt->execute();
+        $users = $stmt->fetch();
+
+        // Display all users and functionality
+    ?>
+</body>
+</html>
