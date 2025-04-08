a932da02932f0fe16c9455b6b0139df26095cd03
Daniel Schager <149520413+dschager@users.noreply.github.com>
2025-04-08 13:14:17 -0400

    Update manageUsers.php

--- public/manageUsers.php
+++ public/manageUsers.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Manage Users</title>
-    <link rel="stylesheet" href="resources/css/introPages.css">
+    <link rel="stylesheet" href="../resources/css/introPages.css">
 </head>
 <body>
     <?php
