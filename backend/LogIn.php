34c7874f7f6e9b61de84f197b38332daa5edaf78
Daniel Schager <149520413+dschager@users.noreply.github.com>
2025-04-07 22:33:50 -0400

    Update LogIn.php

--- backend/LogIn.php
+++ backend/LogIn.php
@@ -71,7 +71,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="introPages.css">
+    <link rel="stylesheet" href="resources/css/introPages.css">
     <title>Log In</title>
 </head>
 <body>
