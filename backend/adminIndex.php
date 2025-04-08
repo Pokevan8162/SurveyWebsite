6a02db0504e8e94efa04a21e0b4feb0f61740a98
Daniel Schager <149520413+dschager@users.noreply.github.com>
2025-04-07 22:34:29 -0400

    Update adminIndex.php

--- backend/adminIndex.php
+++ backend/adminIndex.php
@@ -10,7 +10,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Administrator Home Page</title>
-	<link rel="stylesheet" href="introPages.css">
+	<link rel="stylesheet" href="resources/css/introPages.css">
 </head>
 <body>	
 	<img src="https://s3-us-west-2.amazonaws.com/scorestream-team-profile-pictures/285522/20181011000648_310_mascot1280Near.png" alt="Logo" class="logo">
