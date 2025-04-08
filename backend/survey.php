d619d6a8c40adae78d9b5ab1f0e6956df2d16ab9
Daniel Schager <149520413+dschager@users.noreply.github.com>
2025-04-07 22:36:42 -0400

    Update survey.php

--- backend/survey.php
+++ backend/survey.php
@@ -34,7 +34,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="survey.css"> 
+    <link rel="stylesheet" href="resources/css/survey.css"> 
     
     <title>Questionnaire</title>
     <style>
