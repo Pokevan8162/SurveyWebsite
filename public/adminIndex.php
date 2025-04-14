1c72ef4b641d90ce86a5a91f322306b56e64eb8f
B3NK12 <127891518+B3NK12@users.noreply.github.com>
2025-04-14 15:28:29 -0400

    Update adminIndex.php

--- public/adminIndex.php
+++ public/adminIndex.php
@@ -3,14 +3,29 @@
  manage users (update usernames, passwords, etc.), 
  manage surveys (update questions, see results, and post new questions, create new surveys).
 -->
+<?php
+session_start();
 
+try {
+    $conn = new PDO("mysql:host=localhost;dbname=survey_db", 'root', '');
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // session check
+    if (!isset($_SESSION['user_id'])) {
+        echo "<a href=login.php>Please log in to view surveys.</a>";
+        exit;
+    }
+} catch (PDOException $e) {
+    echo 'Database error: ' . $e->getMessage();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Administrator Home Page</title>
-	<link rel="stylesheet" href="../resources/css/introPages.css">
+	<link rel="stylesheet" href="introPages.css">
 </head>
 <body>	
 	<img src="https://s3-us-west-2.amazonaws.com/scorestream-team-profile-pictures/285522/20181011000648_310_mascot1280Near.png" alt="Logo" class="logo">
