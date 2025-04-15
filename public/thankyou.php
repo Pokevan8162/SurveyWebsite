4abe901fbc77770942227e63eb73ecfd13ace29e
Evan Stoller <163201695+Pokevan8162@users.noreply.github.com>
2025-04-15 16:52:04 -0400

    Create thankyou.php - when you answer a random survey

---
+++ public/thankyou.php
@@ -0,0 +1,20 @@
+<?php
+    session_start();
+    require_once __DIR__ . '/../backend/db.php';
+    $userID = $_SESSION['user_id'];
+    $surveyID = $_SESSION['SurveyID'];
+    $reflectedUserID = $_SESSION['reflectedUserID'];
+
+
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        $feeling = $_POST['feeling'];
+        $concern = $_POST['concern'];
+        $answer = "Feeling: " . $feeling . "\nConcern: " . $concern;
+        $stmt = $conn->prepare("INSERT INTO REFLECTIONS (UserID, ReflectedUserID, SurveyID, Answer) VALUES (?, ?, ?, ?)");
+        $stmt->execute([$userID, $reflectedUserID, $surveyID, $answer]);
+    } else {
+        echo "<p>Insertion failed.</p>";
+    }
+    echo "<p>Thanks!</p>";
+    echo "<a href='index.php'>Back Home</a>";
+?>
