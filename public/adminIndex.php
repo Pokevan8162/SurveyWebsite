b844ab9643a49722bad05991b0a3f9965665c94b
B3NK12 <127891518+B3NK12@users.noreply.github.com>
2025-04-15 11:18:57 -0400

    Update adminIndex.php

--- public/adminIndex.php
+++ public/adminIndex.php
@@ -3,9 +3,24 @@
  manage users (update usernames, passwords, etc.), 
  manage surveys (update questions, see results, and post new questions, create new surveys).
 -->
+
 <?php
-require_once __DIR__ . '/../backend/db.php';
+//start session
 session_start();
+require_once __DIR__ . '/../backend/db.php';
+
+try {
+    $conn = new PDO("mysql:host=localhost;dbname=survey_db", 'root', '');
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // session check
+    if (!isset($_SESSION['user_id'])) {
+        echo "<a href=login.php>Please log in.</a>";
+        exit;
+    }
+} catch (PDOException $e) {
+    echo 'Database error: ' . $e->getMessage();
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,7 +28,7 @@ session_start();
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Administrator Home Page</title>
-	<link rel="stylesheet" href="../resources/css/introPages.css">
+	<link rel="stylesheet" href="introPages.css">
 </head>
 <body>	
 	<img src="https://s3-us-west-2.amazonaws.com/scorestream-team-profile-pictures/285522/20181011000648_310_mascot1280Near.png" alt="Logo" class="logo">
@@ -22,7 +37,7 @@ session_start();
 		<div class="title">Welcome to the Administrator Page</div>
 			<a href="manageUsers.php"><button type="button" class="btn">Manage Users</button></a>
 			<a href="viewSurveyResults.php"><button type="button" class="btn">See Survey Results</button></a>
-			<a href="viewSurvey.php"><button type="button" class="btn">Update Surveys</button></a>
+			<a href="viewSurveys.php"><button type="button" class="btn">Update Surveys</button></a>
 			<a href="createSurvey.php"><button type="button" class="btn">Create Survey</button></a>
 			<a href="logout.php"><button type="button" class="btn">Logout</button></a>
 		</div>
