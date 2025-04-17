b1d27d6dfac141eadd2353c098b5fb7171566d49
B3NK12 <127891518+B3NK12@users.noreply.github.com>
2025-04-17 09:25:31 -0400

    Update adminIndex.php

--- public/adminIndex.php
+++ public/adminIndex.php
@@ -4,9 +4,6 @@ session_start();
 require_once __DIR__ . '/../backend/db.php';
 
 try {
-    $conn = new PDO("mysql:host=localhost;dbname=survey_db", 'root', '');
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
     // session check
     if (!isset($_SESSION['user_id'])) {
         echo "<a href=login.php>Please log in.</a>";
@@ -22,7 +19,7 @@ try {
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Administrator Home Page</title>
-	<link rel="stylesheet" href="../resources/css/introPages.css">
+	<link rel="stylesheet" href="introPages.css">
 </head>
 <body>	
 	<a href="adminIndex.php"><img src="logo.png" alt="Logo" class="logo"></a>
@@ -30,7 +27,7 @@ try {
 		<div class="form_area">
 		<div class="title">Welcome to the Administrator Page</div>
 			<a href="manageUsers.php"><button type="button" class="btn">Manage Users</button></a>
-			<a href="viewSurveyResults.php"><button type="button" class="btn">See Survey Results</button></a>
+			<a href="listSurveys.php"><button type="button" class="btn">See Survey Results</button></a>
 			<a href="viewSurveys.php"><button type="button" class="btn">Update Surveys</button></a>
 			<a href="createSurvey.php"><button type="button" class="btn">Create Survey</button></a>
 			<a href="logout.php"><button type="button" class="btn">Logout</button></a>
